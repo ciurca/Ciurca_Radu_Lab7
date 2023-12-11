b7753d9c12206d7d8ecd99b1324cd33a4b7c2892
Radu Ciurca <raduciurca@gmail.com>
2023-12-11 19:26:42 +0200

    ex 14 complet functional

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SQLite;
+using SQLiteNetExtensions.Attributes;
+
 
 namespace Ciurca_Radu_Lab7.Models
 {
@@ -14,5 +16,8 @@ namespace Ciurca_Radu_Lab7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
+
     }
 }
